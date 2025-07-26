48fda07837f97fcab20414374199ddd8685027e6
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2025-07-26 22:05:23 +0600

    refactor: export rotate password function

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
+// Exporting core classes
 export * from './crypto-engine/generateEncryptedKey';
 export * from './crypto-engine/decryptGeneratedKey';
+export * from './crypto-engine/rotatePassword';
+
+// Exporting error classes
 export * from './errors/DecryptionError';
+export * from './errors/PasswordRotationError';
