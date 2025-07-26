e4188a1926ea7ff90d27702bbe63cd542a102eea
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2025-07-27 04:12:00 +0600

    feat: export the recovery password related functions

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,10 @@
 export * from './crypto-engine/generateEncryptedKey';
 export * from './crypto-engine/decryptGeneratedKey';
 export * from './crypto-engine/rotatePassword';
+export * from './crypto-engine/recoverEncryptedKey';
+export * from './crypto-engine/rotatePasswordAfterRecovery';
 
 // Exporting error classes
 export * from './errors/DecryptionError';
 export * from './errors/PasswordRotationError';
+export * from './errors/RecoverEncryptionError';
