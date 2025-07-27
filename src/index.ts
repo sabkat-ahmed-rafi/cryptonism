d162358a23a9e4602f021b6883392f574ef88747
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2025-07-27 19:54:26 +0600

    feat: add the encrytion and decryption secret functions

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ export * from './crypto-engine/decryptGeneratedKey';
 export * from './crypto-engine/rotatePassword';
 export * from './crypto-engine/recoverEncryptedKey';
 export * from './crypto-engine/rotatePasswordAfterRecovery';
+export * from './crypto-engine/encryptSecret';
+export * from './crypto-engine/decryptSecret';
 
 // Exporting error classes
 export * from './errors/DecryptionError';
